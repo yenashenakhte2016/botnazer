4e1728a828029bb68811aa8fca5dc5231cd1fe45
yenashenakhte2016 <mmnsfyrad@gmail.com>
2016-09-04 13:29:32 +0430

    Add files via upload

--- plugins/supergroup.lua
+++ plugins/supergroup.lua
@@ -114,13 +114,13 @@ end
 
 --Get and output info about supergroup
 local function callback_info(cb_extra, success, result)
-local title ="Info for SuperGroup: ["..result.title.."]\n\n"
-local admin_num = "Admin count: "..result.admins_count.."\n"
-local user_num = "User count: "..result.participants_count.."\n"
-local kicked_num = "Kicked user count: "..result.kicked_count.."\n"
-local channel_id = "ID: "..result.peer_id.."\n"
+local title ="اطلاعات سوپر گروه: ["..result.title.."]\n\n"
+local admin_num = "تعداد اددمین ها: "..result.admins_count.."\n"
+local user_num = "تعداد اعضا: "..result.participants_count.."\n"
+local kicked_num = "تعداد افراد ریمو شده: "..result.kicked_count.."\n"
+local channel_id = "آیدی : "..result.peer_id.."\n"
 if result.username then
-	channel_username = "Username: @"..result.username
+	channel_username = "یوزرنیم: @"..result.username
 else
 	channel_username = ""
 end
