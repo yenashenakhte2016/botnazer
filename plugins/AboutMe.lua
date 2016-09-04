fe4cfe8d6a0a0b90ec4950671e866f80185a4f1d
yenashenakhte2016 <mmnsfyrad@gmail.com>
2016-09-04 15:14:56 +0430

    Add files via upload

---
+++ plugins/AboutMe.lua
@@ -0,0 +1,32 @@
+
+do
+
+local function run(msg, matches)
+  if matches[1] == 'bot' then
+    if is_sudo(msg) then
+    send_document(get_receiver(msg), "./data/me/version.webp", ok_cb, false)
+      return "🔖شما اددمین ربات هستید"
+    elseif is_admin1(msg) then
+    send_document(get_receiver(msg), "./data/me/admin.webp", ok_cb, false)
+      return "🔖شما اددمین ربات هستید"
+    elseif is_owner(msg) then
+    send_document(get_receiver(msg), "./data/me/owner.webp", ok_cb, false)
+      return "🔖شما اددمین کل گروه هستید"
+    elseif is_momod(msg) then
+    send_document(get_receiver(msg), "./data/me/mod.webp", ok_cb, false)
+      return "🔖شما مدیر گروه هستید"
+    else
+    send_document(get_receiver(msg), "./data/me/mmbr.webp", ok_cb, false)
+      return "🔖شما یک ممبر ساده هستید"
+    end
+  end
+end
+
+return {
+  patterns = {
+    "^(من کیم)$",
+    "^(من کیم)$"
+    },
+  run = run
+}
+end
